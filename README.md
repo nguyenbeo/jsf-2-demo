5da5f07a483774a6ec73563bafd84a2366ad3348
Nguyen Binh Nguyen <nguyenbeo@gmail.com>
2015-11-04 22:26:59 +0100

    Added demo for managed bean

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # jsf-2-demo
 Some demos of JSF 2
+* Managed bean
+* Ajax
+
+# Technologies
+* JSF 2
+* Servlet 2.5
+* Embeded Jetty
+
+# How to run
+Run:
+mvn install
+mvn jetty:run
+
+Go to:
+http://localhost:9999/jsf-2-demo/index.xhtml
---
+++ src/main/java/com/kthsoft/jsf/bean/DemoManagedBean.java
@@ -0,0 +1,23 @@
+package com.kthsoft.jsf.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class DemoManagedBean implements Serializable {
+	private static final long serialVersionUID = 8185771831360665660L;
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
